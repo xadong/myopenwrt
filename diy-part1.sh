d34615492aade33c809aed87dce55ec014afaa89
xadong <69294149+xadong@users.noreply.github.com>
2022-07-20 23:21:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/xadong/luci-app-ikoolproxy package/luci-app-ikoolproxy
-git clone https://github.com/xadong/luci-app-smartdns package/luci-app-smartdns
-git clone https://github.com/xadong/openwrt-luci-theme package/openwrt-luci-theme
-git clone https://github.com/fw876/helloworld package/helloworld
+git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+git clone https://github.com/kenzok8/small package/small
